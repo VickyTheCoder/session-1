0b134a3bd92d4acbce63c22c455e9547c030ccf4
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2024-09-16 09:43:56 +0530

    Url mapping for homepage

--- FrontEnd/views.py
+++ FrontEnd/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
+def homepage(request):
+    if request.method == 'GET':
+        return render(request=request,
+                      template_name='index.html')
+    raise Http404("Invalid Request")
+
